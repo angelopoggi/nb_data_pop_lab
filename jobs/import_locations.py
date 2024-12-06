c6e3e1f01e39e87fceac1cf9172e6fbd51ad5caf
Angelo Poggi <angelo.poggi@networktocode.com>
2024-12-06 10:31:51 -0500

    initial commit

---
+++ jobs/import_locations.py
@@ -0,0 +1,35 @@
+from nautobot.apps.jobs import Job, register_jobs, FileVar
+from nautobot.dcim.models import Location, LocationType
+import us
+
+class ImportLocations(Job):
+    '''
+    Imports WAYNES LOCATIONS from a CSV
+    '''
+
+    csv_file = FileVar(description="Upload a CSV file containing location data.")
+
+    def _find_state(self, state_two_letters):
+        state = us.states.lookup(str(state_two_letters))
+        return state.name
+
+    def _get_location_type(self, site_name):
+        '''
+        pass in the site name and return datacenter or branch
+        :param site_name:
+        :return: string
+        '''
+        if site_name.endswith("-DC"):
+            #query NB data to get location Type UUID
+            location_type = LocationType.objects.get(name="Data Center")
+        elif site_name.endswith("-BR"):
+            location_type = LocationType.objects.get(name="Data Center")
+        if location_type:
+            return location_type
+        else:
+            self.logger.error("unable to find location type")
+
+    def run(self, data, commit):
+
+
+
